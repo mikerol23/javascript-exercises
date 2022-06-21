51d1b1dcac58320642f4d29b14cdde44e1243438
mikerol23 <michaelrolincik@gmail.com>
2022-06-21 16:55:54 -0400

    add repeat string loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (inputString) {
+  let splitInput = inputString.split("");
+  let reverseArray = splitInput.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
