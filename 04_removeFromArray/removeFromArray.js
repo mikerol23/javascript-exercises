08ee0a0bfc659ad8b6e356bb6dc81f8e89e24707
mikerol23 <michaelrolincik@gmail.com>
2022-07-13 14:11:55 -0400

    Create function to remove single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...toRemove) {
+  let index = inputArray.indexOf(...toRemove);
+  inputArray.splice(index, 1);
+  return inputArray;
 };
 
 // Do not edit below this line
