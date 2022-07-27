b628eb39849314a2c0b0ef0ecb23213847c9b0b9
mikerol23 <michaelrolincik@gmail.com>
2022-07-27 18:26:46 -0400

    Add funciton to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let sumTotal = 0;
+  if (b > a) {
+    for (let i = 0; i <= Math.abs(a - b); i++) {
+      let newAdd = a + i;
+      sumTotal += newAdd;
+    }
+    return sumTotal;
+  }
+  if (a > b) {
+    for (let i = 0; i <= Math.abs(a - b); i++) {
+      let newAdd = b + i;
+      sumTotal += newAdd;
+    }
+  }
+  return sumTotal;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
