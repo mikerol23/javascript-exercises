6a8c6840d13cf83fae9b87b4547043210327cc4c
mikerol23 <michaelrolincik@gmail.com>
2022-07-28 12:11:01 -0400

    Add basic ftoc()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let c = (f - 32) * (5 / 9);
+  return c;
 };
 
-const ctof = function() {
-
-};
+const ctof = function () {};
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
