2fa0e50ea6c1ac357def3a66680191223a2dbf49
mikerol23 <michaelrolincik@gmail.com>
2022-07-28 10:21:17 -0400

    Fix divisible by 400 test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,13 @@
 const leapYears = function (year) {
-  if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) return true;
-  if (year % 4 == 0 && year % 100 !== 0) return false;
-  return false;
+  if (
+    year % 4 == 0 &&
+    (year % 100 !== 0 || (year % 100 == 0 && year % 400 == 0))
+  )
+    return true;
+  else return false;
+  //if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) return true;
+  // if (year % 4 == 0 && year % 100 !== 0) return false;
+  // return false;
 };
 
 // Do not edit below this line
